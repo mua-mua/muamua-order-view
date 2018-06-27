d4882b91f8a2710310a47ca2154115b43dc97a42
林亮佑 <yashi884@yashi884.local>
2018-06-27 21:10:44 +0900

    vuexを追加

--- shop/src/main.js
+++ shop/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import store from './store.js'
 import App from './App.vue'
 import Router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex);
 new Vue({
   router: Router,
+  store,
   render: h => h(App)
 }).$mount('#app')
